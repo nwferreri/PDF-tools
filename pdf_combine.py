c980201858333ed61b19e81abdf38411a4de35b7
Nicholas Wildason-Ferreri <nickferreri@Nicks-iMac-2017.local>
2023-09-01 14:37:27 -0400

    Complete overhaul

--- pdf_combine.py
+++ pdf_combine.py
@@ -1,15 +1,28 @@
-import PyPDF2
-import sys
+# Combines PDFs given by the user into one document
 
-inputs = sys.argv[1:]  # Includes as many arguments as are passed
-
-
-def merge_pdfs(pdf_list):
-    merger = PyPDF2.PdfFileMerger()
-    for pdf in pdf_list:
-        merger.append(pdf)
-    merger.write('super.pdf')
-    merger.close()
+# Import tools
+from pypdf import PdfWriter
 
 
-merge_pdfs(inputs)
+# Open PDF writer
+merger = PdfWriter()
+
+# Parse user input into list of files and append them
+while True:
+    try:
+        user_input = input("Enter files to merge, separated by commas: ")
+        pdf_list = user_input.split(',')
+        for pdf in pdf_list:
+            merger.append(pdf)
+    except FileNotFoundError:
+        print("One or more files not found. Please try again.\n"
+            "TIP: Separate files by commas only, no spaces.")
+    else:
+        break 
+
+# Get output filepath and write it
+out_file = input("Enter output PDF filename: ")
+if out_file[-4:] != '.pdf':
+    out_file = out_file + '.pdf'
+merger.write(out_file)
+merger.close()
