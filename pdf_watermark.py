a5f29a744bb3c724fc36e4dece58477d8e756eae
Nick Wildason-Ferreri <nickferreri@Nicks-iMac-2017.local>
2023-09-01 15:21:23 -0400

    Complete overhaul

--- pdf_watermark.py
+++ pdf_watermark.py
@@ -1,13 +1,35 @@
-import PyPDF2
+# Adds a watermark to all pages in a PDF
 
-template = PyPDF2.PdfFileReader(open('super.pdf', 'rb'))
-watermark = PyPDF2.PdfFileReader(open('wtr.pdf', 'rb'))
-output = PyPDF2.PdfFileWriter()
+# Import tools
+from pypdf import PdfWriter, PdfReader
 
-for i in range(template.getNumPages()):
-    page = template.getPage(i)
-    page.mergePage(watermark.getPage(0))
-    output.addPage(page)
 
-    with open('watermarked_output.pdf', 'wb') as file:
-        output.write(file)
+# Get watermark from user
+while True:
+    try:
+        wm_file = input("Enter watermark PDF filename: ")
+        watermark = PdfReader(wm_file).pages[0]
+    except FileNotFoundError:
+        print("ERROR: File does not exist.")
+    else:
+        break
+
+# Get document to be watermarked from user
+while True:
+    try:
+        doc = input("Enter PDF filename: ")
+        writer = PdfWriter(clone_from=doc)
+    except FileNotFoundError:
+        print("ERROR: File does not exist.")
+    else:
+        break
+
+# Add watermark to all pages
+for page in writer.pages:
+    page.merge_page(watermark, over=False)
+
+# Get output filepath and write it
+out_file = input("Enter output PDF filename: ")
+if out_file[-4:] != '.pdf':
+    out_file = out_file + '.pdf'
+writer.write(out_file)
